6b6d4263f75570b252c0da4fa93a647912f1f096
heosangbeom <luv_3_u@naver.com>
2024-12-14 14:08:46 +0900

    ✨feat(config): mustache 설정 추가
    - mustache 설정
      - 뷰리졸버
    
    - 유틸
      - 뷰리졸버 관련 문자열 상수화

--- security-service/src/main/kotlin/com/tokkaiiii/account/securityservice/config/WebMvcConfig.kt
+++ security-service/src/main/kotlin/com/tokkaiiii/account/securityservice/config/WebMvcConfig.kt
@@ -1,21 +1,25 @@
 package com.tokkaiiii.account.securityservice.config
 
+import com.tokkaiiii.account.securityservice.util.PREFIX
+import com.tokkaiiii.account.securityservice.util.SUFFIX
+import com.tokkaiiii.account.securityservice.util.TEXT_HTML
+import com.tokkaiiii.account.securityservice.util.UTF_8
 import org.springframework.boot.web.servlet.view.MustacheViewResolver
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType.TEXT_HTML_VALUE
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import kotlin.text.Charsets.UTF_8
 
 @Configuration
 class WebMvcConfig : WebMvcConfigurer{
 
     override fun configureViewResolvers(registry: ViewResolverRegistry) {
         val resolver = MustacheViewResolver()
-        resolver.setCharset(UTF_8.name())
-        resolver.setContentType(TEXT_HTML_VALUE)
-        resolver.setPrefix("classpath:/templates/")
-        resolver.setSuffix(".html")
+
+        resolver.setCharset(UTF_8)
+        resolver.setContentType(TEXT_HTML)
+        resolver.setPrefix(PREFIX)
+        resolver.setSuffix(SUFFIX)
         registry.viewResolver(resolver)
     }
 }
+
---
+++ security-service/src/main/kotlin/com/tokkaiiii/account/securityservice/util/ViewResolverConstant.kt
@@ -0,0 +1,6 @@
+package com.tokkaiiii.account.securityservice.util
+
+const val UTF_8 = "UTF-8"
+const val TEXT_HTML = "text/html;charset=UTF-8"
+const val PREFIX = "classpath:/templates/"
+const val SUFFIX = ".html"
