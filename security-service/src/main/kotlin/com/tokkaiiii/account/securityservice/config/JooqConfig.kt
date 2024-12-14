b4f950e86887d26c1f98c75abb58194b5c9fe99d
heosangbeom <luv_3_u@naver.com>
2024-12-14 13:46:23 +0900

    ✨feat(config): jOOQ 설정 추가
    - config
      - jOOQ config 기능 추가

---
+++ security-service/src/main/kotlin/com/tokkaiiii/account/securityservice/config/JooqConfig.kt
@@ -0,0 +1,19 @@
+package com.tokkaiiii.account.securityservice.config
+
+import org.jooq.conf.ExecuteWithoutWhere.THROW
+import org.springframework.boot.autoconfigure.jooq.DefaultConfigurationCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class JooqConfig {
+
+    @Bean
+    fun jooqDefaultConfigurationCustomizer(): DefaultConfigurationCustomizer =
+        DefaultConfigurationCustomizer { customizer ->
+            customizer.settings()
+                .withExecuteDeleteWithoutWhere(THROW)
+                .withExecuteUpdateWithoutWhere(THROW)
+                .withRenderSchema(false)}
+
+}
