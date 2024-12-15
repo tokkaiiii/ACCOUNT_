d8b9c5d39daf0e1dfe439d4fd7eeccdf0e528f2d
heosangbeom <luv_3_u@naver.com>
2024-12-15 13:41:39 +0900

    ✨refactor(security): formLogin 람다 수정
    - formLogin 람다 수정
      - it 으로 표시한 것을 이해하기 쉽게 구체적으로 formLoginConfigurer 명시

--- security-service/src/main/kotlin/com/tokkaiiii/account/securityservice/config/SecurityConfig.kt
+++ security-service/src/main/kotlin/com/tokkaiiii/account/securityservice/config/SecurityConfig.kt
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.config.http.SessionCreationPolicy.*
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
@@ -14,7 +12,7 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfig {
 
     @Bean
-    fun passwordEncoder()= BCryptPasswordEncoder()
+    fun passwordEncoder() = BCryptPasswordEncoder()
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -24,15 +22,17 @@ class SecurityConfig {
             .httpBasic { it.disable() }
             .authorizeHttpRequests { requests ->
                 requests
-                    .requestMatchers("/user/**").authenticated()  // /admin/** 경로에 대해서 ADMIN 역할만 접근 가능
-                    .requestMatchers("/manager/**").hasAnyRole("ADMIN", "MANAGER")  // /user/** 경로에 대해 USER 또는 ADMIN 역할만 접근 가능
+                    .requestMatchers("/user/**")
+                    .authenticated()  // /admin/** 경로에 대해서 ADMIN 역할만 접근 가능
+                    .requestMatchers("/manager/**")
+                    .hasAnyRole("ADMIN", "MANAGER")  // /user/** 경로에 대해 USER 또는 ADMIN 역할만 접근 가능
                     .requestMatchers("/admin/**").hasRole("ADMIN")
                     .anyRequest().permitAll()  // 나머지 요청은 허용
             }
-            .formLogin {
-                it.loginPage("/loginForm")
-                it.loginProcessingUrl("/login") // login 주소가 호출되면 시큐리티가 낚아채서 대신 로그인 진행
-                    .defaultSuccessUrl("/") // 로그인 성공하면 / 메인으로 보내기
+            .formLogin { formLoginConfigurer ->
+                formLoginConfigurer.loginPage("/loginForm")
+                formLoginConfigurer.loginProcessingUrl("/login") // login 주소가 호출되면 시큐리티가 낚아채서 대신 로그인 진행
+                formLoginConfigurer.defaultSuccessUrl("/") // 로그인 성공하면 / 메인으로 보내기
             }
             .build()
     }
